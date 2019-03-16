5538db08e5984d649e1e0174fe37e7dd93fcf1dd
Akash Mozumdar <akashmozumdar@gmail.com>
2019-03-16 11:33:39 -0400

    finish implementation and make some dummy posts

--- loader.js
+++ loader.js
@@ -4,4 +4,33 @@ xhr.send();
 xhr.onload = function () {
 	if (!xhr.responseText.startsWith("<!DOCTYPE html>"))
 		document.getElementById("blog").innerHTML = xhr.responseText;
-}
+};
+
+document.getElementById("archive").querySelectorAll("h3").forEach(function (element) {
+	element.style.cursor = "pointer";
+	var list = null;
+	element.onclick = function () {
+		if (list) {
+			document.getElementById("archive").removeChild(list);
+			list = null;
+		} else {
+			list = document.createElement("ul");
+			var currentPosts = posts[element.innerText];
+			if (currentPosts && currentPosts.forEach) {		
+				currentPosts.forEach(function (link) {
+					list.appendChild(document.createElement("li")).innerHTML = link;
+				});
+			}
+			element.insertAdjacentElement("afterend", list);
+		}
+	};
+});
+
+var posts = {
+	"March 2019": [
+		"<a href=\"?iile.html\">Immediately Invoked Lambda Expressions in C++</a>"
+	],
+	"February 2019": [
+		"<a href=\"?hello_world.html\">Hello World!</a>"
+	]
+};
